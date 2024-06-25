19ed4e3daabefd231a0c78d224cc66a19b893551
PierreGauv <pierregauvin@hotmail.fr>
2024-06-25 09:34:37 +0200

    navBar

---
+++ src/templates/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar">
+    <div>
+        <div class="">
+            <img src="" width="" alt="Menu">
+        </div>
+        <div class="">
+            <img src="" width="" alt="Logo Online Training">
+        </div>
+        <div class="">
+        <div class="">
+            <img src="" width="" alt="Rechercher">
+        </div>
+        <div class="">
+            <img src="" width="" alt="Compte">
+        </div>
+        <div class="">
+            <img src="" width="" alt="Panier">
+        </div>
+        </div>
+    </div>
+</nav>
+
+</body>
+</html>
