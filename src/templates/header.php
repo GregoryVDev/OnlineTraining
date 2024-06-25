a0e3cc5c2a2c7886ce6c5813c2e8c095bcc0bc28
PierreGauv <pierregauvin@hotmail.fr>
2024-06-25 10:01:22 +0200

    image navBar

--- src/templates/header.php
+++ src/templates/header.php
@@ -1,32 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
 <nav class="navbar">
     <div>
         <div class="">
-            <img src="" width="" alt="Menu">
+            <img src="./img/navBar/menuBurger.png" width="" alt="Menu">
         </div>
         <div class="">
-            <img src="" width="" alt="Logo Online Training">
+            <img src="./img/navBar/logo-online-training.png" width="" alt="Logo Online Training">
         </div>
+
         <div class="">
-        <div class="">
-            <img src="" width="" alt="Rechercher">
-        </div>
-        <div class="">
-            <img src="" width="" alt="Compte">
-        </div>
-        <div class="">
-            <img src="" width="" alt="Panier">
-        </div>
-        </div>
+            <div><img src="./img/navBar/iconSearch.png" width="" alt="Rechercher"></div>
+            <div><img src="./img/navBar/account.svg" width="" alt="Compte"></div>
+            <div><img src="./img/navBar/cart.svg" width="" alt="Panier"> </div>
+         </div>
     </div>
 </nav>
-
-</body>
-</html>
