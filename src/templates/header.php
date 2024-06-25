7e634416a2a593468601eab71145b12ddd58ea6d
PierreGauv <pierregauvin@hotmail.fr>
2024-06-25 10:30:13 +0200

    css navBar

--- src/templates/header.php
+++ src/templates/header.php
@@ -1,16 +1,18 @@
-<nav class="navbar">
-    <div>
+
+
+<nav>
+    <div class="navbar">
         <div class="">
-            <img src="./img/navBar/menuBurger.png" width="" alt="Menu">
+            <img src="./img/navBar/menuBurger.png" width="20px" alt="Menu">
         </div>
         <div class="">
-            <img src="./img/navBar/logo-online-training.png" width="" alt="Logo Online Training">
+            <img src="./img/navBar/logo-online-training.png" width="60px" alt="Logo Online Training">
         </div>
 
-        <div class="">
-            <div><img src="./img/navBar/iconSearch.png" width="" alt="Rechercher"></div>
-            <div><img src="./img/navBar/account.svg" width="" alt="Compte"></div>
-            <div><img src="./img/navBar/cart.svg" width="" alt="Panier"> </div>
+        <div class="navBar_droite">
+            <div><img src="./img/navBar/iconSearch.png" width="11px" alt="Rechercher"></div>
+            <div><img src="./img/navBar/account.svg" width="11px" alt="Compte"></div>
+            <div><img src="./img/navBar/cart.svg" width="11px" alt="Panier"> </div>
          </div>
     </div>
 </nav>
