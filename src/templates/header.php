283fc53d1fbe68649ee431bca284bb7b696d80ed
PierreGauv <pierregauvin@hotmail.fr>
2024-06-25 16:21:17 +0200

    responsive et css navBar

--- src/templates/header.php
+++ src/templates/header.php
@@ -9,17 +9,15 @@
             <img src="./img/navBar/logo-online-training.png" width="80px" alt="Logo Online Training">
         </div>
         <div class="rubrique">
-        <div><a href="">TENDANCE</a></div>
-        <div><a href="">FEMMME</a></div>
-        <div><a href="">HOMME</a></div>
-        <div><a href="">PETITS PRIX</a></div>
-
+            <div><a href="">TENDANCE</a></div>
+            <div class="menuCouleur"><a href="">FEMMME</a></div>
+            <div><a href="">HOMME</a></div>
+            <div><a href="">PETITS PRIX</a></div>
         </div>
-
         <div class="navBar_droite">
             <div><img src="./img/navBar/iconSearch.png" width="15px" alt="Rechercher"></div>
             <div><img src="./img/navBar/account.svg" width="15px" alt="Compte"></div>
             <div><img src="./img/navBar/cart.svg" width="15px" alt="Panier"></div>
-         </div>
+        </div>
     </div>
 </nav>
