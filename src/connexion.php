e8f470f2d30a41812eae3fcc965964a5efe45613
GregoryVDev <arexo@hotmail.fr>
2024-06-26 15:34:14 +0200

    Added the connection to connect to the database

--- src/connexion.php
+++ src/connexion.php
@@ -11,16 +11,15 @@ if (!empty($_POST)) {
     // Le formulaire a été envoyé
     // On vérifie que TOUS les champs requis sont remplis
 
-    if (isset($_POST["email"], $_POST_["pass"]) && !empty($_POST["email"]) && !empty($_POST["pass"])) {
+    if (isset($_POST["email"], $_POST["pass"]) && !empty($_POST["email"]) && !empty($_POST["pass"])) {
         // On vérifie que l'email en est un
         if (!validateEmail($_POST["email"])) {
             die("L'adresse email est incorrecte");
         }
 
         // On se connecte à la bdd
-        require_once("connect.php");
+        require_once("./connect.php");
 
-        // Assuming $db is the PDO instance from "connect.php"
         $sql = "SELECT * FROM users WHERE email = :email";
 
         $query = $db->prepare($sql);
@@ -32,13 +31,13 @@ if (!empty($_POST)) {
         $user = $query->fetch();
 
         if (!$user) {
-            die("The user and/or password is incorrect");
+            die("L'utilisateur et/ou le mot de passe est incorrect");
         }
 
         // On a un user existant, on peut vérifier son mdp
 
         if (!password_verify($_POST['pass'], $user["pass"])) {
-            die("The user and/or password is incorrect");
+            die("L'utilisateur et/ou le mot de passe est incorrect");
         }
 
         // L'utilisateur et le mot de passe sont corrects
@@ -48,7 +47,8 @@ if (!empty($_POST)) {
         // On stocke dans $_SESSION les informations de l'utilisateur
         $_SESSION['user'] = [
             "id" => $user['id'],
-            "pseudo" => $user['username'],
+            "nom" => $user['nom'],
+            "prenom" => $user['prenom'],
             "email" => $user['email'],
             "roles" => $user['roles']
         ];
@@ -57,7 +57,7 @@ if (!empty($_POST)) {
         header("Location: index.php");
     } else {
         // Formulaire incomplet
-        die("The form is incomplete");
+        die("Le formulaire est incomplet");
     }
 }
 
