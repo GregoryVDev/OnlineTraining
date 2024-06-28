df21c1d6b22c1f0708f2139559b71abbd5e7c658
cy-fa <cypher_fab@proton.me>
2024-06-28 15:00:28 +0200

    v bd

--- src/templates/navbar_dashboard.php
+++ src/templates/navbar_dashboard.php
@@ -12,7 +12,7 @@
     <a href="../produits/dashboard_produits.php"><button type="button" class="btn btn-outline-primary">produits</button></a>
     <a href="../categories/dashboard_categories.php"><button type="button" class="btn btn-outline-primary">categories</button></a>
     <a href="../commandes/dashboard_commandes.php"><button type="button" class="btn btn-outline-primary">commandes</button></a>
-    <a href="../messagerie/dashboard_messagerie.php"><button type="button" class="btn btn-outline-primary">messagerie</button></a>
+    <a href="../../messagerie.php"><button type="button" class="btn btn-outline-primary">messagerie</button></a>
     <a href="../panier/dashboard_panier.php"><button type="button" class="btn btn-outline-primary">panier</button></a>
     <a href="../users/dashboard_users.php"><button type="button" class="btn btn-outline-primary">utilisateur</button></a>
 </div>
