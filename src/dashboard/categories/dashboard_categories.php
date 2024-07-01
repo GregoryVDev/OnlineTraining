8b314dd15d55a5b2d602be29a308fdfc6e01faa9
cy-fa <cypher_fab@proton.me>
2024-07-01 17:01:27 +0200

    v modif produit

--- src/dashboard/categories/dashboard_categories.php
+++ src/dashboard/categories/dashboard_categories.php
@@ -29,7 +29,7 @@ $categories = $query->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../../css/dashboard/dashboard.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Dashboard</title>
 </head>
@@ -72,11 +72,11 @@ $categories = $query->fetchAll(PDO::FETCH_ASSOC);
                 ?>
 
                 <div>
-
-                    <a classe="deco" href="./create_categories.php"><button type="button" class="btn btn-outline-secondary">Ajouter une categorie</button></a>
+                    <div class="add">
+                        <a classe="deco" href="./create_categories.php"><button type="button" class="btn btn-outline-secondary">AJOUTER UNE CATEGORIE</button></a>
+                    </div>
                     <br>
                     <br>
-
                 </div>
             </tbody>
         </table>
--- src/dashboard/users/dashboard_users.php
+++ src/dashboard/users/dashboard_users.php
@@ -14,7 +14,7 @@ $users = $query->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../../css/dashboard/dashboard.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Dashboard_users</title>
 </head>
@@ -60,12 +60,10 @@ $users = $query->fetchAll(PDO::FETCH_ASSOC);
 
                 ?>
 
-                <div>
-
-                    <a classe="deco" href="./create_users.php"><button type="button" class="btn btn-outline-secondary">Ajouter un utilisateur</button></a>
+                <div class="add">
+                    <a classe="deco" href="./create_users.php"><button type="button" class="btn btn-outline-secondary">AJOUTER UN UTILISATEUR</button></a>
                     <br>
                     <br>
-
                 </div>
             </tbody>
         </table>
