8f72a885ed9fc7f9dc1afd56a327d0ba7e87b037
cy-fa <cypher_fab@proton.me>
2024-07-01 15:25:25 +0200

    v css dashboard

--- src/dashboard/produits/dashboard_produits.php
+++ src/dashboard/produits/dashboard_produits.php
@@ -29,7 +29,7 @@ $produits = $query->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../../css/dashboard/dashboard.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Dashboard</title>
 </head>
@@ -91,12 +91,10 @@ $produits = $query->fetchAll(PDO::FETCH_ASSOC);
 
                 ?>
 
-                <div>
-
-                    <a classe="deco" href="../produits/create_produits.php"><button type="button" class="btn btn-outline-secondary">Ajouter un produit</button></a>
+                <div class="add">
+                    <a href="../produits/create_produits.php"><button type="button" class="btn btn-outline-secondary">Ajouter un produit</button></a>
                     <br>
                     <br>
-
                 </div>
             </tbody>
         </table>
