577f7ca7a48e637c485d3f3741f219ebc246eb81
PierreGauv <pierregauvin@hotmail.fr>
2024-07-01 16:54:04 +0200

    css footer

--- src/templates/footer.php
+++ src/templates/footer.php
@@ -1,5 +1,28 @@
 <footer>
 
+    <div class="dely_map_secure">
+        <div class="icon_footer">
+            <a href=""><img class="icon_footer_align" src="./img/footer/delivery01.png" title="Livraison"
+                    alt="Livraison"></a>
+            <div class="text_icon_footer">
+                <p class="footer-p">Livraison</p>
+            </div>
+        </div>
+        <div class="icon_footer"><a href=""><img class="icon_footer_align" src="./img/footer/placeMarker01.png"
+                    title="Nos Magasins" alt="Points de vente"></a>
+            <div class="text_icon_footer">
+                <p class="footer-p">Nos magasins</p>
+
+            </div>
+        </div>
+        <div class="icon_footer"><a href=""><img class="icon_footer_align" src="./img/footer/CheckLock01.png"
+                    title="Paiement Sécurisé" alt="Paiement sécurisé"></a>
+            <div class="text_icon_footer">
+                <p class="footer-p">Paiement sécurisé</p>
+            </div>
+        </div>
+    </div>
+
     <h1 class="footer-h1">NEWSLETTER</h1>
 
     <h3 class="footer-h3">Recevoir les actus mode et les offres d’Online !</h3>
@@ -27,26 +50,5 @@
 <div><img src="./img/footer/tic-tac.png" width="20px" alt="Logo"></div>
 </div> -->
 
-    <div class="dely_map_secure">
-        <div class="icon_footer">
-            <a href=""><img class="icon_footer_align" src="./img/footer/delivery.png" title="Livraison"
-                    alt="Livraison"></a>
-            <div class="text_icon_footer">
-                <p class="footer-p">Livraison</p>
-            </div>
-        </div>
-        <div class="icon_footer"><a href=""><img class="icon_footer_align" src="./img/footer/placeMarker.png"
-                    title="Nos Magasins" alt="Points de vente"></a>
-            <div class="text_icon_footer">
-                <p class="footer-p">Nos magasins</p>
 
-            </div>
-        </div>
-        <div class="icon_footer"><a href=""><img class="icon_footer_align" src="./img/footer/CheckLock.png"
-                    title="Paiement Sécurisé" alt="Paiement sécurisé"></a>
-            <div class="text_icon_footer">
-                <p class="footer-p">Paiment sécurisé</p>
-            </div>
-        </div>
-    </div>
 </footer>
