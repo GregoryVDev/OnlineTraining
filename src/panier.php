7c2964b08f0d63d006f1baf03d0a2f8c3e10bbf8
PierreGauv <pierregauvin@hotmail.fr>
2024-07-04 16:34:49 +0200

    panier

--- src/panier.php
+++ src/panier.php
@@ -20,6 +20,45 @@ foreach ($panier as $produit) {
     $total += $produit['prix_ht'] * $produit['quantite'];
 }
 
+// Gestion de la suppression d'une quantité spécifique
+if (isset($_POST['remove_id']) && isset($_POST['remove_quantite'])) {
+    $remove_id = $_POST['remove_id'];
+    $remove_quantite = (int)$_POST['remove_quantite'];
+
+    foreach ($panier as &$produit) {
+        if ($produit['id'] == $remove_id) {
+            if ($produit['quantite'] <= $remove_quantite) {
+                // Supprimer le produit du panier si la quantité est inférieure ou égale à la quantité à supprimer
+                unset($panier[$remove_id]);
+            } else {
+                // Réduire la quantité
+                $produit['quantite'] -= $remove_quantite;
+            }
+            $_SESSION['panier'] = array_values($panier);
+            break;
+        }
+    }
+
+    header("Location: panier.php");
+    exit();
+}
+
+// Gestion de la suppression d'un produit
+if (isset($_POST['delete_id'])) {
+    $delete_id = $_POST['delete_id'];
+
+    foreach ($panier as $key => $produit) {
+        if ($produit['id'] == $delete_id) {
+            unset($panier[$key]);
+            $_SESSION['panier'] = array_values($panier);
+            break;
+        }
+    }
+
+    header("Location: panier.php");
+    exit();
+}
+
 include('./templates/requete_navbar_menu_catalogue.php');
 ?>
 
@@ -45,35 +84,48 @@ include('./templates/requete_navbar_menu_catalogue.php');
             <?php if (count($panier) > 0): ?>
             <?php foreach ($panier as $produit): ?>
             <div class="recap_panier">
-                <div class="produit_colonne">
-                    <div>
-                        <img src="<?= escape($produit['image_produit']) ?>" width="100px"
-                            alt="<?= escape($produit['nom_produit']) ?>">
-                    </div>
-                    <div class="recap_text">
-                        <p><?= escape($produit['nom_produit']) ?></p>
-                        <p><?= escape($produit['prix_ht']) ?> €</p>
-                        <p><?= escape($produit['couleur']) ?></p>
-                        <p><?= escape($produit['taille']) ?></p>
-                        <p>Quantité : <?= escape($produit['quantite']) ?></p>
-                    </div>
+                <div>
+                    <img src="<?= escape($produit['image_produit']) ?>" width="100px"
+                        alt="<?= escape($produit['nom_produit']) ?>">
+                </div>
+                <div class="recap_text">
+                    <p><?= escape($produit['nom_produit']) ?></p>
+                    <p><?= escape($produit['prix_ht']) ?>€</p>
+                    <p><?= escape($produit['couleur']) ?></p>
+                    <p><?= escape($produit['taille']) ?></p>
+                    <p>Quantité : <?= escape($produit['quantite']) ?></p>
+
+                    <!-- Formulaire pour supprimer une quantité spécifique -->
+                    <form method="post" action="panier.php" style="display:inline;">
+                        <input type="hidden" name="remove_id" value="<?= escape($produit['id']) ?>">
+                        <input type="number" name="remove_quantite" value="1" min="1" required>
+                        <button type="submit" class="remove-quantity-btn">Supprimer 1</button>
+                    </form>
+
+                    <!-- Formulaire pour supprimer un produit entier -->
+                    <form method="post" action="panier.php" style="display:inline;">
+                        <input type="hidden" name="delete_id" value="<?= escape($produit['id']) ?>">
+                        <button type="submit" class="delete-btn">Supprimer le produit</button>
+                    </form>
                 </div>
             </div>
             <?php endforeach; ?>
             <?php else: ?>
             <p>Votre panier est vide.</p>
             <?php endif; ?>
-        </div>
-        <div class="details_panier">
-            <div class="total_cde">
-                <p>TOTAL DE MA COMMANDE</p>
-                <p class="price"><?= escape($total) ?>€</p>
+
+            <div class="details_panier">
+                <div class="total_cde">
+                    <p>TOTAL DE MA COMMANDE</p>
+                    <p class="price"><?= escape($total) ?>€</p>
+                </div>
+                <div class="commande">
+                    <form method="post" action="commande.php">
+                        <button type="submit" class="cde">COMMANDER</button>
+                    </form>
+                </div>
             </div>
-            <div class="commande">
-                <input class="cde" type="submit" value="COMMANDER"></input>
-            </div>
         </div>
-    </div>
     </div>
 
     <div class="aussi">
