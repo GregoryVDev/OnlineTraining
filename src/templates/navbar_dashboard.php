a57d82dd1218d43aab19763d645df9bb0108c927
cy-fa <cypher_fab@proton.me>
2024-07-11 11:45:10 +0200

    v dasboard commande

--- src/templates/navbar_dashboard.php
+++ src/templates/navbar_dashboard.php
@@ -2,11 +2,11 @@
 
 <div class="contenair_nav_dash">
     <div class="nav_dash">
+        <a href="../users/dashboard_users.php"><button type="button" class="btn btn-outline-secondary">UTILISATEUR</button></a>
         <a href="../produits/dashboard_produits.php"><button type="button" class="btn btn-outline-secondary">PRODUIT</button></a>
         <a href="../categories/dashboard_categories.php"><button type="button" class="btn btn-outline-secondary">CATEGORIE</button></a>
         <a href="../commandes/dashboard_commandes.php"><button type="button" class="btn btn-outline-secondary">COMMANDE</button></a>
         <a href="../../messagerie.php"><button type="button" class="btn btn-outline-secondary">MESSAGERIE</button></a>
-        <a href="../panier/dashboard_panier.php"><button type="button" class="btn btn-outline-secondary">PANIER</button></a>
-        <a href="../users/dashboard_users.php"><button type="button" class="btn btn-outline-secondary">UTILISATEUR</button></a>
+        <a href="../../index.php"><button type="button" class="btn btn-outline-secondary">INDEX</button></a>
     </div>
 </div>
