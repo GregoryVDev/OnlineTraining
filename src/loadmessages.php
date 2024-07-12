586f6564a099d289117149a678a92f0faf9a0794
PierreGauv <pierregauvin@hotmail.fr>
2024-07-12 09:09:44 +0200

    nom messagerie

--- src/loadmessages.php
+++ src/loadmessages.php
@@ -7,38 +7,27 @@ require_once("./connect.php");
 
 $current_user_id = $_SESSION["user"]["user_id"];
 
-$sql = "SELECT *, DATE_FORMAT(CONVERT_TZ(time, '+01:00', '+02:00'), '%H:%i') AS formatted_time FROM messagerie ORDER BY time ASC";
+// Requête SQL pour joindre les tables messagerie et users et récupérer les prénoms
+$sql = "
+    SELECT m.*, u.prenom, DATE_FORMAT(CONVERT_TZ(m.time, '+00:00', '+02:00'), '%H:%i') AS formatted_time
+    FROM messagerie m
+    JOIN users u ON m.user_id = u.id
+    ORDER BY m.time ASC
+";
+
 $query = $db->prepare($sql);
 $query->execute();
 
 while ($message = $query->fetch(PDO::FETCH_ASSOC)) {
     $message_class = ($message["user_id"] == $current_user_id) ? 'sent' : 'received';
 ?>
-    <div class="message <?= $message_class ?>">
-        <p><?= $message["message"]; ?></p>
-        <small>
-            <div class="message_time"><?= (new DateTime($message['time']))->format('d/m/Y H:i'); ?></div>
-        </small>
-
-
-    </div>
+<div class="message <?= $message_class ?>">
+    <p><strong><?= htmlspecialchars($message["prenom"]); ?> :</strong>
+        <?= nl2br(htmlspecialchars($message["message"])); ?></p>
+    <small>
+        <div class="message_time"><?= (new DateTime($message['time']))->format('d/m/Y H:i'); ?></div>
+    </small>
+</div>
 <?php
 }
-
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Messagerie instantanée</title>
-</head>
-
-<body>
-
-</body>
-
-</html>
